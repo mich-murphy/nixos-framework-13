96aed62bf77c81f1e65d05a2e161c33e0f90424b
Michael Murphy <github@elmurphy.com>
2026-05-16 10:43:46 +1000

    fix(wezterm): reducing font size

--- modules/home/terminals/wezterm.lua
+++ modules/home/terminals/wezterm.lua
@@ -82,7 +82,7 @@ config.switch_to_last_active_tab_when_closing_tab = true
 -- General
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("0xProto Nerd Font")
-config.font_size = 10
+config.font_size = 9.5
 config.use_dead_keys = false
 config.adjust_window_size_when_changing_font_size = false
 config.window_close_confirmation = "NeverPrompt"
